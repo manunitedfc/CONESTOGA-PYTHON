6145b4c53d90b35bed2f120264545d4478c12863
Thomas Gofar <gofarthomas96@gmail.com>
2023-04-09 12:29:24 -0400

    class and input check

--- Project.py
+++ Project.py
@@ -1,3 +1,43 @@
+#This program aim to build cars based on user input
+#It will also act as a customization agent, which will 
+#Get input from users for their desired upgrades/build
 
-print("hi")
-print("bye")
+#---Input Check---
+def stringCheck(input):
+    if input.isdigit() is False and input!="":
+        valid = True
+    else:
+        print(f"Invalid Input")
+        valid = False
+    return valid
+def digitCheck(input):
+    if input.isdigit() and input!="":
+        valid = True
+    else:
+        print(f"Invalid Input")
+        valid = False
+    return valid
+
+#Introduce to User Program Goal
+print(f"Hello, this Program will help create your dream car lot")
+
+#Create Car Class with User Info 
+class Car:
+    def __init__(self,make,model,trim,year,colour,drivetrain,mileage):
+        #instance attributes
+        self.make = make
+        self.model = model
+        self.trim = trim
+        self.year = year
+        self.colour = colour
+        self.drivetrain = drivetrain
+        self.mileage = mileage
+
+
+
+
+
+
+
+
+# GET USER CAR
