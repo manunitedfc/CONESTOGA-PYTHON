ad0f8650790e7b205df4465528b26cf0f7b32cbb
Thomas Gofar <gofarthomas96@gmail.com>
2023-04-09 13:55:57 -0400

    Get user car data

--- Project.py
+++ Project.py
@@ -23,7 +23,7 @@ print(f"Hello, this Program will help create your dream car lot")
 
 #Create Car Class with User Info 
 class Car:
-    def __init__(self,make,model,trim,year,colour,drivetrain,mileage):
+    def __init__(self,make,model,trim,year,colour,drivetrain,mileage,age):
         #instance attributes
         self.make = make
         self.model = model
@@ -32,12 +32,53 @@ class Car:
         self.colour = colour
         self.drivetrain = drivetrain
         self.mileage = mileage
-
-
-
-
-
-
-
-
+#New line
+print()
 # GET USER CAR
+
+def getuserCar():
+    valid = False
+    #Get User Input
+    while valid is False:
+        while valid is False:
+            make = str(input("Enter Make: ").strip().capitalize())
+            if stringCheck(make) is False:
+                break
+            model = str(input("Enter Model: ").strip().capitalize)
+            if stringCheck(model) is False:
+                break
+            trim = str(input("Enter Trim (or Blank): ").strip().upper())
+            if trim == "":
+                trim = None
+            year = str(input("Enter Year: ").strip())
+            if digitCheck(year) is False:
+                break
+            colour = str(input("Enter Colour: ").strip().capitalize())
+            if stringCheck(colour) is False:
+                break
+            #Get Drivetrain
+            print("\n1 - FWD\n2 - RWD\n3 - AWD\n")
+            option = str(input("Enter Drivetrain Option (1/2/3): "))
+            match option:
+                case "1":
+                    drivetrain = "FWD"
+                case "2":
+                    drivetrain = "RWD"
+                case "3":
+                    drivetrain = "AWD"
+                case _:
+                    break
+            #Get Mileage
+            mileage = str(input("Enter Desired Mileage (e.g., 1000): ").strip())
+            if digitCheck(mileage) is False:
+                break
+            valid = True
+            print()
+
+
+getuserCar()
+
+
+
+
+#print to file (user choice)
